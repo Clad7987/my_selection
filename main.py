a611679bd741490293e514f52bc294d72f4f36e3
Clad7987 <vicenters.10@gmail.com>
2024-10-29 08:16:15 -0300

    update v1.54

--- main.py
+++ main.py
@@ -9,14 +9,64 @@ def main(obj=None, is_first_run=True):
     data_path.mkdir(exist_ok=True)
     path = pathlib.Path("./imgs")
     if is_first_run:
-        data = {"images": []}
-        for image in os.listdir(path):
-            data["images"].append(
-                {
-                    "path": str(path / image),
-                    "create_at": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                }
-            )
+        data = {
+            "tumblr": {"images": [], "videos": []},
+            "reddit": {"images": [], "videos": []},
+            "unknow": {"images": [], "videos": []},
+        }
+
+        for item in os.listdir(data_path):
+            if "tumblr" in item.get("filename").lower():
+                if "mp4" in item.get("filename").lower():
+                    data["tumblr"]["videos"].append(
+                        {
+                            "path": str(path / item.get("filename")),
+                            "create_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                            "type": "video",
+                        }
+                    )
+                else:
+                    data["tumblr"]["images"].append(
+                        {
+                            "path": str(path / item.get("filename")),
+                            "create_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                            "type": "image",
+                        }
+                    )
+            elif "rdt" in item.get("filename").lower():
+                if "mp4" in item.get("filename").lower():
+                    data["reddit"]["videos"].append(
+                        {
+                            "path": str(path / item.get("filename")),
+                            "create_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                            "type": "video",
+                        }
+                    )
+                else:
+                    data["reddit"]["images"].append(
+                        {
+                            "path": str(path / item.get("filename")),
+                            "create_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                            "type": "image",
+                        }
+                    )
+            else:
+                if "mp4" in item.get("filename").lower():
+                    data["unknow"]["videos"].append(
+                        {
+                            "path": str(path / item.get("filename")),
+                            "create_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                            "type": "video",
+                        }
+                    )
+                else:
+                    data["unknow"]["images"].append(
+                        {
+                            "path": str(path / item.get("filename")),
+                            "create_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                            "type": "image",
+                        }
+                    )
 
         with open(data_path / "data.json", "w") as file:
             json.dump(data, file)
@@ -25,15 +75,64 @@ def main(obj=None, is_first_run=True):
             data = json.load(file)
 
         for item in obj:
-            data["images"].append(
-                {
-                    "path": str(path / item.get("filename")),
-                    "create_at": item.get("date"),
-                }
-            )
+            if "tumblr" in item.get("filename").lower():
+                if "mp4" in item.get("filename").lower():
+                    data["tumblr"]["videos"].append(
+                        {
+                            "path": str(path / item.get("filename")),
+                            "create_at": item.get("date"),
+                            "type": "video",
+                        }
+                    )
+                else:
+                    data["tumblr"]["images"].append(
+                        {
+                            "path": str(path / item.get("filename")),
+                            "create_at": item.get("date"),
+                            "type": "image",
+                        }
+                    )
+            elif "rdt" in item.get("filename").lower():
+                if "mp4" in item.get("filename").lower():
+                    data["reddit"]["videos"].append(
+                        {
+                            "path": str(path / item.get("filename")),
+                            "create_at": item.get("date"),
+                            "type": "video",
+                        }
+                    )
+                else:
+                    data["reddit"]["images"].append(
+                        {
+                            "path": str(path / item.get("filename")),
+                            "create_at": item.get("date"),
+                            "type": "image",
+                        }
+                    )
+            else:
+                if "mp4" in item.get("filename").lower():
+                    data["unknow"]["videos"].append(
+                        {
+                            "path": str(path / item.get("filename")),
+                            "create_at": item.get("date"),
+                            "type": "video",
+                        }
+                    )
+                else:
+                    data["unknow"]["images"].append(
+                        {
+                            "path": str(path / item.get("filename")),
+                            "create_at": item.get("date"),
+                            "type": "image",
+                        }
+                    )
 
         with open(data_path / "data.json", "w") as file:
             json.dump(data, file)
+
+        import util
+
+        util.remove_duplicated()
 
 
 if __name__ == "__main__":
---
+++ util.py
@@ -0,0 +1,98 @@
+import json
+from datetime import datetime
+
+
+def add_propierty(propierty, value):
+    with open("data/data.json", "r") as file:
+        data = json.load(file)
+
+    for item in data["images"]:
+        index = data["images"].index(item)
+        data["images"][index][propierty] = value
+
+    with open("data/data.json", "w") as file:
+        json.dump(data, file)
+
+
+def remove_duplicated():
+    with open("data/data.json", "r") as file:
+        data = json.load(file)
+
+    is_old_data = False
+    if is_old_data:
+        # Categorize into image and video
+        new_data = {}
+        new_data["tumblr"] = {
+            "images": [
+                item
+                for item in data["images"]
+                if item["type"] == "image" and "tumblr" in item["path"].lower()
+            ],
+            "videos": [
+                item
+                for item in data["images"]
+                if item["type"] == "video" and "tumblr" in item["path"].lower()
+            ],
+        }
+        new_data["reddit"] = {
+            "images": [
+                item
+                for item in data["images"]
+                if item["type"] == "image" and "rdt" in item["path"].lower()
+            ],
+            "videos": [
+                item
+                for item in data["images"]
+                if item["type"] == "video" and "rdt" in item["path"].lower()
+            ],
+        }
+        new_data["unknow"] = {
+            "images": [
+                item
+                for item in data["images"]
+                if item["type"] == "image"
+                and (
+                    "rdt" not in item["path"].lower()
+                    and "tumblr" not in item["path"].lower()
+                )
+            ],
+            "videos": [
+                item
+                for item in data["images"]
+                if item["type"] == "video"
+                and (
+                    "rdt" not in item["path"].lower()
+                    and "tumblr" not in item["path"].lower()
+                )
+            ],
+        }
+    else:
+        new_data = data
+
+    def filter_oldest(items):
+        unique_items = {}
+        for item in items:
+            path = item["path"]
+            create_at = datetime.strptime(item["create_at"], "%Y-%m-%d %H:%M:%S")
+
+            if path in unique_items:
+                existing_date = datetime.strptime(
+                    unique_items[path]["create_at"], "%Y-%m-%d %H:%M:%S"
+                )
+                if create_at < existing_date:
+                    unique_items[path] = item
+            else:
+                unique_items[path] = item
+
+        return list(unique_items.values())
+
+    for source in new_data.keys():
+        new_data[source]["images"] = filter_oldest(new_data[source]["images"])
+        new_data[source]["videos"] = filter_oldest(new_data[source]["videos"])
+
+    with open("data.json", "w") as file:
+        json.dump(new_data, file)
+
+
+if __name__ == "__main__":
+    remove_duplicated()
