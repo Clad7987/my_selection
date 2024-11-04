15e03e3ef346d46bd9a1a34700874ef8af2d646b
ryuvi <vicenters10@gmail.com>
2024-11-04 13:42:57 -0300

    some update

--- push.py
+++ push.py
@@ -1,5 +1,8 @@
 import subprocess as sp
+import shlex
+
 
 sp.run(["python", "-m", "venv", "venv"])
-sp.run([".\venv\Scripts\activate"])
-sp.run(["pip", "install", "-r", "requirements,txt"])
+sp.run(shlex.split(".\\venv\\Scripts\\activate && pip install -r requirements.txt"))
+# sp.run([".\\venv\\Scripts\\activate", "&&", ""])
+# sp.run(["pip", "install", "-r", "requirements,txt"])
--- util.py
+++ util.py
@@ -94,5 +94,52 @@ def remove_duplicated():
         json.dump(new_data, file)
 
 
+def add_special():
+    with open("data/data.json", "r") as file:
+        data = json.load(file)
+
+    specials_vids = [
+        '<iframe width="854" height="480" src="https://thothub.to/embed/817801" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="222" height="124.875" src="https://thothub.to/embed/43288" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="852" height="480" src="https://thothub.to/embed/809282" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="852" height="480" src="https://thothub.to/embed/840798" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="270" height="151.875" src="https://thothub.to/embed/35490" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="268" height="150.75" src="https://thothub.to/embed/38593" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="852" height="480" src="https://thothub.to/embed/903107" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="852" height="480" src="https://thothub.to/embed/829130" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="270" height="151.875" src="https://thothub.to/embed/518481" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="272" height="153" src="https://thothub.to/embed/901677" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="270" height="151.875" src="https://thothub.to/embed/589638" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="854" height="480" src="https://thothub.to/embed/1184235" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="852" height="480" src="https://thothub.to/embed/250744" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="854" height="480" src="https://thothub.to/embed/571406" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="270" height="151.875" src="https://thothub.to/embed/840354" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="270" height="151.875" src="https://thothub.to/embed/978330" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="852" height="480" src="https://thothub.to/embed/831573" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="854" height="480" src="https://thothub.to/embed/620664" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="854" height="480" src="https://thothub.to/embed/659270" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="852" height="480" src="https://thothub.to/embed/160905" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="852" height="480" src="https://thothub.to/embed/300446" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="850" height="480" src="https://thothub.to/embed/268157" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="270" height="151.875" src="https://thothub.to/embed/933193" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="852" height="480" src="https://thothub.to/embed/270778" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="270" height="151.875" src="https://thothub.to/embed/873539" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="852" height="480" src="https://thothub.to/embed/910824" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="270" height="151.875" src="https://thothub.to/embed/70992" frameborder="0" allowfullscreen></iframe>',
+        '<iframe width="270" height="151.875" src="https://thothub.to/embed/789929" frameborder="0" allowfullscreen></iframe>',
+        ''
+    ]
+    specials_imgs = []
+    today =  datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    data['special'] = {
+        "videos": [{"path": vid, "create_at": today, "type": "video"} for vid in specials_vids],
+        "images": [{"path": img, "create_at": today, "type": "image"} for img in specials_imgs],
+    }
+
+    with open("data/data.json", "w") as file:
+        json.dump(data, file)
+
+
 if __name__ == "__main__":
-    remove_duplicated()
+    # remove_duplicated()
+    add_special()
